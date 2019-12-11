7b4e01032975e30ab88d7f23e082087a8af9c4b6
Nawfel <nawfel_abbassi@outlook.fr>
2019-12-10 23:39:26 -0500

    second commit

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -14,5 +14,23 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("fixme", Items[0].Name);
         }
+
+        [Test]
+        public void HavingSellInZeroAndQualityEqualFive_UpdateQualityResultEqualToOne()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(1, Items[0].Quality);
+        }
+
+        [Test]
+        public void HavingOneSellInAndQualityEqualFive_UpdateQualityResultEqualToThree()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 1, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(3, Items[0].Quality);
+        }
     }
 }
