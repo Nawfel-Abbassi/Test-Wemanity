e65a4e3b1a0ed20f93a38f24e088e172fb35eb31
Nawfel <nawfel_abbassi@outlook.fr>
2019-12-17 23:02:56 -0500

    test commit

--- GildedRose.cs
+++ GildedRose.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-
 namespace csharp
 {
     public class GildedRose
@@ -14,86 +13,94 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-
-                        if (Items[i].Name == "Conjured Mana Cake")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
+                ManageQuality(i);
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
-
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-
-                                if (Items[i].Name == "Conjured Mana Cake")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    ManageQuality(i);
                 }
             }
         }
+
+        #region private methods
+
+        private void ManageQuality(int i)
+        {
+            if (Items[i].Name != "Aged Brie")
+            {
+                Items[i].Quality = IncreaseOrDegradeQuality(Items[i].Name, Items[i].Quality, Items[i].SellIn);
+            }
+            else
+            {
+                Items[i].Quality = IncreaseQuality(Items[i].Quality);
+            }
+        }
+
+        private int DegradeQuality(string name, int quality)
+        {
+            if (quality > 0)
+            {
+                if (name != "Sulfuras, Hand of Ragnaros")
+                {
+                    quality = quality - 1;
+                }
+                if (name == "Conjured Mana Cake")
+                {
+                    quality = quality - 1;
+                }
+            }
+            return quality;
+        }
+
+        private int IncreaseQualityBackstage(int sellIn, int quality)
+        {
+            if (sellIn­­ > 0)
+            {
+                if (quality < 50)
+                {
+                    quality = quality + 1;
+                    if (sellIn < 11)
+                    {
+                        quality = IncreaseQuality(quality);
+                    }
+                    if (sellIn < 6)
+                    {
+                        quality = IncreaseQuality(quality);
+                    }
+                }
+            }
+            else
+            {
+                quality = 0;
+            }
+            return quality;
+        }
+
+        public int IncreaseQuality(int quality)
+        {
+            if (quality < 50)
+            {
+                quality = quality + 1;
+            }
+            return quality;
+        }
+
+        private int IncreaseOrDegradeQuality(string name, int quality, int sellIn)
+        {
+            if (name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                quality = DegradeQuality(name, quality);
+            }
+            else
+            {
+                quality = IncreaseQualityBackstage(sellIn, quality);
+            }
+            return quality;
+        }
+        #endregion
     }
 }
